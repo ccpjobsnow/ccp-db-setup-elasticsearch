fc6d2a104109fbfa9bcfb36560a7df35ac8360a9
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2023-12-20 23:55:15 -0300

    ajustes

--- src/main/java/com/ccp/implementations/db/setup/elasticsearch/ElasticSearchDbSetupCreator.java
+++ src/main/java/com/ccp/implementations/db/setup/elasticsearch/ElasticSearchDbSetupCreator.java
@@ -1,8 +1,11 @@
 package com.ccp.implementations.db.setup.elasticsearch;
 
+import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpFolderDecorator;
 import com.ccp.decorators.CcpStringDecorator;
+import com.ccp.decorators.CcpTimeDecorator;
 import com.ccp.especifications.db.setup.CcpDbSetupCreator;
+import com.ccp.especifications.http.CcpHttpResponse;
 
 class ElasticSearchDbSetupCreator implements CcpDbSetupCreator {
 	
@@ -27,6 +30,24 @@ class ElasticSearchDbSetupCreator implements CcpDbSetupCreator {
 	public void dropAllTables() {
 		String uri = "/_all";
 		this.httpRequester.executeHttpRequest(uri, "DELETE", "", 200);
+	}
+
+	@Override
+	public void deleteAllData() {
+		String url = "/_all/_delete_by_query";
+		
+		String matchAll = CcpConstants.EMPTY_JSON
+						.putSubKey("query", "match_all", CcpConstants.EMPTY_JSON)
+						.asJson();
+		
+		CcpHttpResponse endpointResponse = this.httpRequester.executeHttpRequest(url, "POST", matchAll);
+
+		if (endpointResponse.isClientError()) {
+			new CcpTimeDecorator().sleep(1000);
+			this.deleteAllData();
+			return;
+		}
+		
 	}
 	
 
